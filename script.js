782577a5d6c0c89f3458f03c89dbd12eb23f86bb
Prateek <106758759+Prateek7071@users.noreply.github.com>
2026-02-01 19:55:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-//your code here!
+const ol = document.getElementById('infi-list')
+let count = 10
+ol.addEventListener('scrollend',()=>{
+ addLi()
+ addLi() 
+})
 
+function addLi(){
+  count++;
+  let childLi = document.createElement('li')
+  childLi.innerText=`Item ${count}`
+  ol.appendChild(childLi)
+}
